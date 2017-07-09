70bd61d23fd3dbb4f379dbc7225cc4f7ffc56ed0
Anton Matosov <anton.matosov@gmail.com>
2017-07-08 20:55:16 -0700

    Switch to recomended way of building

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class HalarduinoConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        self.run('cmake . %s' % (cmake.command_line))
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
